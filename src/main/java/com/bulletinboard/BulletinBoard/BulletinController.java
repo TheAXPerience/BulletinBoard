b3b9810893516c72e17750f11b3c61558a686a8e
Alvin Xu <alvin3xu1@gmail.com>
2023-06-22 21:32:25 -0700

    added support for editing and deleting posts

--- src/main/java/com/bulletinboard/BulletinBoard/BulletinController.java
+++ src/main/java/com/bulletinboard/BulletinBoard/BulletinController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bulletinboard.BulletinBoard.PostingController.PostingRequest;
@@ -30,6 +33,8 @@ public class BulletinController {
 	public String createForm(Model model) {
 		model.addAttribute("posting", new Posting());
 		model.addAttribute("hello", "Create New Post");
+		model.addAttribute("link", "/submit");
+		model.addAttribute("method", null);
 		return "form";
 	}
 	
@@ -40,6 +45,35 @@ public class BulletinController {
 		newPosting.setContent(posting.content());
 		this.postingService.savePosting(newPosting);
 		
-		return "redirect:";
+		return "redirect:/";
+	}
+	
+	@GetMapping("/{postingId}/edit")
+	public String editForm(@PathVariable("postingId") long postingId, Model model) {
+		Posting post = this.postingService.getPostingById(postingId);
+		model.addAttribute("posting", post);
+		model.addAttribute("hello", "Edit Post #" + postingId);
+		model.addAttribute("link", "/" + postingId + "/submit");
+		model.addAttribute("method", "PUT");
+		return "form";
+	}
+	
+	@PutMapping("/{postingId}/submit")
+	public String submitEditForm(
+			Model model,
+			@PathVariable("postingId") long id,
+			@ModelAttribute("posting") PostingRequest posting) {
+		Posting newPosting = this.postingService.getPostingById(id);
+		newPosting.setPoster(posting.poster());
+		newPosting.setContent(posting.content());
+		this.postingService.savePosting(newPosting);
+		
+		return "redirect:/";
+	}
+	
+	@DeleteMapping("/{postingId}/delete")
+	public String deleteForm(@PathVariable("postingId") long postingId) {
+		this.postingService.deletePosting(postingId);
+		return "redirect:/";
 	}
 }
